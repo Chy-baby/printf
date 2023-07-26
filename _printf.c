3ec7547eea5bb9ed58b7e1362cf96177a6b667c1
Chy-baby <chideraikpeama@gmail.com>
2023-07-25 20:27:28 -0700

    “_printf.c”

--- _printf.c
+++ _printf.c
@@ -31,7 +31,7 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
+print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
@@ -61,6 +61,5 @@ void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+*buff_ind = 0;
 }
